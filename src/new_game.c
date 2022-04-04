0b8d32a0088701468826f5faa5cb381507bea3f4
Tearisten <test@gmail.com>
2022-04-04 00:59:40 -0500

    Change spawn room

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(STARTER_ROOM), MAP_NUM(STARTER_ROOM), WARP_ID_NONE, 5, 9);
     WarpIntoMap();
 }
 
--- src/overworld.c
+++ src/overworld.c
@@ -1534,8 +1534,6 @@ void CB2_NewGame(void)
     PlayTimeCounter_Start();
     ScriptContext1_Init();
     ScriptContext2_Disable();
-    gFieldCallback = ExecuteTruckSequence;
-    gFieldCallback2 = NULL;
     DoMapLoadLoop(&gMain.state);
     SetFieldVBlankCallback();
     SetMainCallback1(CB1_Overworld);
