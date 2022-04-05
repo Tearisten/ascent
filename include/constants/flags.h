b8585313fe3eba4afef93152f2cd416e2bf27188
Tearisten <test@gmail.com>
2022-04-05 15:34:36 -0500

    Finish room 1&2 warps and flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1221,7 +1221,7 @@
 #define FLAG_FIRE_STARTER                                           0x494 // Unused Flag
 #define FLAG_WATER_STARTER                                          0x495 // Unused Flag
 #define FLAG_GRASS_STARTER                                          0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
+#define FLAG_ALL_STARTERS                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
