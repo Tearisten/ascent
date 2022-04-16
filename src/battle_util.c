e7ccd8e9afb63bac1af16492f9a4a3622cf363f4
Tearisten <test@gmail.com>
2022-04-16 01:33:51 -0500

    Buff overcoat to use ice/sand def/spdef boost

--- src/battle_util.c
+++ src/battle_util.c
@@ -8965,11 +8965,13 @@ static u32 CalcDefenseStat(u16 move, u8 battlerAtk, u8 battlerDef, u8 moveType, 
     }
 
     // sandstorm sp.def boost for rock types
-    if (IS_BATTLER_OF_TYPE(battlerDef, TYPE_ROCK) && WEATHER_HAS_EFFECT && gBattleWeather & B_WEATHER_SANDSTORM && !usesDefStat)
-        MulModifier(&modifier, UQ_4_12(1.5));
+    if ((IS_BATTLER_OF_TYPE(battlerDef, TYPE_ROCK) || GetBattlerAbility(battlerDef) == ABILITY_OVERCOAT)
+        && WEATHER_HAS_EFFECT && gBattleWeather & B_WEATHER_SANDSTORM && !usesDefStat)
+            MulModifier(&modifier, UQ_4_12(1.5));
 
     // hail boosts def for ice types
-    if (IS_BATTLER_OF_TYPE(battlerDef, TYPE_ICE) && WEATHER_HAS_EFFECT && gBattleWeather & B_WEATHER_HAIL && usesDefStat)
+    if ((IS_BATTLER_OF_TYPE(battlerDef, TYPE_ICE) || GetBattlerAbility(battlerDef) == ABILITY_OVERCOAT)
+        && WEATHER_HAS_EFFECT && gBattleWeather & B_WEATHER_HAIL && usesDefStat)
         MulModifier(&modifier, UQ_4_12(1.5));
 
     // The defensive stats of a Player's Pokémon are boosted by x1.1 (+10%) if they have the 5th badge and 7th badges.
